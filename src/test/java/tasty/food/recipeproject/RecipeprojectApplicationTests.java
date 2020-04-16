a99820fb7f3aa91dc2c56e15cb1991824b4020bd
Prasad730 <prasad1998shinde@gmail.com>
2020-04-16 10:25:23 +0530

    Added Index page

---
+++ src/test/java/tasty/food/recipeproject/RecipeprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package tasty.food.recipeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
